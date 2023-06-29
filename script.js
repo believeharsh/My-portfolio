64787b48b6341c4130e13dca171ae92a2238d049
Bontinue <93024harsh@gmail.com>
2023-06-29 21:45:59 +0530

    I was working on random fancy feature on the master

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const tablinks = document.getElementsByClassName('tab-links');
-const tabcontent = document.getElementsByClassName('tab-contents');
 
-function opentab(tabname) {
-    for (tablinks of tablink) {
-        tablinks.classList.remove("active-link")
-
-    }
-    for (tabcontents of tabcontent) {
-        tabcontents.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById()
-}
