44bfa86694624f498aff24107c456f552e37fc3f
Bontinue <93024harsh@gmail.com>
2023-07-22 16:27:09 +0530

    added some tages and reviewd the page

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 
-// SOCIAL PANEL JS
-const side_menu_container = document.querySelector(".page-container");
+// // SOCIAL PANEL JS
+// const side_menu_container = document.querySelector(".page-container");
 
-const Main_Menu_button = document.getElementById("Main-Menu-button");
+// const Main_Menu_button = document.getElementById("Main-Menu-button");
 
-Main_Menu_button.addEventListener("click", () => {
-    side_menu_container.classList.toggle("visible");
-});
+// Main_Menu_button.addEventListener("click", () => {
+//     side_menu_container.classList.toggle("visible");
+// });
 
 
