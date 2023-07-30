8761d6c16f1df60fe6a696d4c4c5b17c374940f7
Bontinue <93024harsh@gmail.com>
2023-07-30 15:38:04 +0530

    formated the style.css file

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
+// Side bar feature script 
+const side_menu_container = document.querySelector(".page-container");
+
+const Main_Menu_button = document.getElementById("Main-Menu-button");
+
+Main_Menu_button.addEventListener("click", () => {
+  side_menu_container.classList.toggle("visible");
+});
 
 
-// // SOCIAL PANEL JS
-// const side_menu_container = document.querySelector(".page-container");
+// about me feature script
 
-// const Main_Menu_button = document.getElementById("Main-Menu-button");
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
 
-// Main_Menu_button.addEventListener("click", () => {
-//     side_menu_container.classList.toggle("visible");
-// });
+function opentab(tabname) {
+  for (let tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (let tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
 
+
+
+// var sidemenu = document.getElementById("side-menu");
+// function openmenu() {
+//   sidemenu.style.right = "0";
+// }
+// function closemenu() {
+//   sidemenu.style.right = "-200px";
+// }
+
+// for viewing the messages on your google sheets 
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwSWWaECApk3NJOruxqPrxBpY0coMh5A9bT3lsUmq9PO4xvSFTc9SblpTaEawxYT8amvg/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
 
